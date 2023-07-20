f0176acac8505bd0f6d67d9b9bc903073ce2b60d
Carlos Avila <cavilart21@gmail.com>
2023-07-20 16:08:54 +0200

    setup projects and experience

---
+++ src/components/technologies/Tech.svelte
@@ -0,0 +1,57 @@
+<section class="flex flex-row flex-wrap justify-center gap-10">
+	<div class="bg-white rounded-full p-2 w-24 flex content-center align-middle justify-center">
+		<figure>
+			<img src="/images/css3.png" alt="CSS 3" width="100" height="100" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/html5.png" alt="HTML5 3" width="100" height="100" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/docker.png" alt="DOCKER" width="100" height="100" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24 overflow-hidden">
+		<figure>
+			<img src="/images/javascript.png" alt="JAVASCRIPT" width="100" height="100" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/mongo.png" alt="MONGO" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/nextjs.png" alt="NEXTJS" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/nodejs.png" alt="NODEJS" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/react.png" alt="REACT" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/redux.png" alt="REDUX" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/svelte.png" alt="SVELTE" />
+		</figure>
+	</div>
+	<div class="bg-white rounded-full p-2 w-24">
+		<figure>
+			<img src="/images/webpack.png" alt="WEBPACK" />
+		</figure>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,13 @@
 	import DiNodejsSmall from 'svelte-icons/di/DiNodejsSmall.svelte';
 	import DiFirebase from 'svelte-icons/di/DiFirebase.svelte';
 	import MdChatBubble from 'svelte-icons/md/MdChatBubble.svelte';
+
 	import Tilt from '$lib/tilt';
 	import Card from '../components/card/Card.svelte';
 	import Hero from '../components/hero/Hero.svelte';
 	import PageContent from '../components/pageContent/PageContent.svelte';
+	import Timeline from '../components/timeline/Timeline.svelte';
+	import Tech from '../components/technologies/Tech.svelte';
 </script>
 
 <Hero title="Page" subtitle="This is a page">
@@ -49,4 +52,28 @@
 	</section>
 </PageContent>
 
-<PageContent title="Work Experience" subtitle="What I've done so far" />
+<PageContent title="Work Experience" subtitle="What I've done so far">
+	<Timeline />
+	<Tech />
+</PageContent>
+
+<PageContent
+	title="Projects"
+	subtitle="My Work"
+	description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error inventore sit velit harum, delectus, maiores quis repudiandae odio omnis atque enim reprehenderit facilis ex rem voluptas, perferendis dolor quo saepe autem? Nisi laudantium quasi architecto aut alias repudiandae, corporis voluptate quo repellat ut saepe voluptatem pariatur laboriosam, numquam totam quidem?"
+>
+	<section class="sm:px-16 px-6 sm:py-20 py-10 max-w-7xl mx-auto relative z-0 mt-0">
+		<div class="card w-96 bg-base-100 shadow-xl">
+			<figure><img src="/images/project-a.png" alt="Shoes" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">Website</h2>
+				<p>Funny web site</p>
+				<div class="card-actions justify-end">
+					<span class="text-primary">#react</span>
+					<span class="text-secondary">#mongodb</span>
+					<span class="text-accent">#tailwind</span>
+				</div>
+			</div>
+		</div>
+	</section>
+</PageContent>
